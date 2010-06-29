9fb3b57dcf100514a3e99bf062c6f8ace0cff5d5
Geoff evans <geoff@flummoxed.org>
2010-06-29 15:48:07 -0700

    move Luna to root

--- Utilities/Luna/Graph/Base64Encode.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdlib.h>
-
-const char *Base64Encode(void *in, size_t in_len);
-void *Base64Decode(const char *in, size_t *out_len);
-
--- Utilities/Luna/Scene/SceneInit.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Luna/API.h"
-
-namespace Luna
-{
-  LUNA_SCENE_API void SceneInitialize();
-  LUNA_SCENE_API void SceneCleanup();
-}

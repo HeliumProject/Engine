1fdb7d695681c2ef2ffffcca5488133075b5c767
Geoff evans <geoff@flummoxed.org>
2010-07-07 12:12:46 -0700

    toolbar redo pt 1.

--- Luna/Scene/ToolsPanel.cpp
+++ Luna/Scene/ToolsPanel.cpp
@@ -1,7 +1,5 @@
 #include "Precompile.h"
 #include "ToolsPanel.h"
-
-#include "CameraModeButton.h"
 #include "SceneEditor.h"
 
 #include "Application/UI/ArtProvider.h"
@@ -13,92 +11,44 @@ ToolsPanel::ToolsPanel( SceneEditor* sceneEditor, wxWindowID id, const wxPoint& 
 : wxPanel( sceneEditor, id, pos, size, style )
 , m_SceneEditor( sceneEditor )
 {
-  SetMinSize( size );
+    SetMinSize( size );
 
-	wxBoxSizer* mainSizer;
-	mainSizer = new wxBoxSizer( wxVERTICAL );
-	
-	wxFlexGridSizer* autoFlexSizer;
-	autoFlexSizer = new Nocturnal::AutoFlexSizer();
-	autoFlexSizer->SetFlexibleDirection( wxBOTH );
-	autoFlexSizer->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
-	
-#if ( 1 )
+    wxBoxSizer* mainSizer;
+    mainSizer = new wxBoxSizer( wxVERTICAL );
 
-	m_button10 = new wxBitmapButton( this, wxID_ANY, wxArtProvider::GetBitmap( Nocturnal::ArtIDs::Unknown, wxART_OTHER, wxSize( 32, 32 ) ) );
-	autoFlexSizer->Add( m_button10, 0, wxALL, 0 );
+    wxFlexGridSizer* autoFlexSizer;
+    autoFlexSizer = new Nocturnal::AutoFlexSizer();
+    autoFlexSizer->SetFlexibleDirection( wxBOTH );
+    autoFlexSizer->SetNonFlexibleGrowMode( wxFLEX_GROWMODE_SPECIFIED );
 
-	m_button11 = new wxBitmapButton( this, wxID_ANY, wxArtProvider::GetBitmap( Nocturnal::ArtIDs::Unknown, wxART_OTHER, wxSize( 32, 32 ) ) );
-	autoFlexSizer->Add( m_button11, 0, wxALL, 0 );
-	
-	m_button12 = new wxBitmapButton( this, wxID_ANY, wxArtProvider::GetBitmap( Nocturnal::ArtIDs::Unknown, wxART_OTHER, wxSize( 32, 32 ) ) );
-	autoFlexSizer->Add( m_button12, 0, wxALL, 0 );
-	
-	m_button13 = new wxBitmapButton( this, wxID_ANY, wxArtProvider::GetBitmap( Nocturnal::ArtIDs::Unknown, wxART_OTHER, wxSize( 32, 32 ) ) );
-	autoFlexSizer->Add( m_button13, 0, wxALL, 0 );
-	
-	m_button14 = new wxBitmapButton( this, wxID_ANY, wxArtProvider::GetBitmap( Nocturnal::ArtIDs::Unknown, wxART_OTHER, wxSize( 32, 32 ) ) );
-	autoFlexSizer->Add( m_button14, 0, wxALL, 0 );
-	
-	m_button15 = new wxBitmapButton( this, wxID_ANY, wxArtProvider::GetBitmap( Nocturnal::ArtIDs::Unknown, wxART_OTHER, wxSize( 32, 32 ) ) );
-	autoFlexSizer->Add( m_button15, 0, wxALL, 0 );
-	
-	m_button16 = new wxBitmapButton( this, wxID_ANY, wxArtProvider::GetBitmap( Nocturnal::ArtIDs::Unknown, wxART_OTHER, wxSize( 32, 32 ) ) );
-	autoFlexSizer->Add( m_button16, 0, wxALL, 0 );
+    wxBitmapButton* btn = new wxBitmapButton( this, wxID_ANY, wxArtProvider::GetBitmap( Nocturnal::ArtIDs::Unknown, wxART_OTHER, wxSize( 32, 32 ) ), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT | wxBU_AUTODRAW );
+    autoFlexSizer->Add( btn, 0, wxALL, 0 );
+    mainSizer->Add( autoFlexSizer, 0, wxEXPAND, 5 );
 
-  autoFlexSizer->Add( new CameraModeButton( this, m_SceneEditor ), 0, wxALL, 0 );
+    m_Divider = new wxStaticLine( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL );
+    mainSizer->Add( m_Divider, 0, wxBOTTOM|wxEXPAND|wxTOP, 5 );
 
-#elif ( 0 )
+    wxBoxSizer* bottomSizer;
+    bottomSizer = new wxBoxSizer( wxVERTICAL );
 
-	m_button10 = new wxButton( this, wxID_ANY, wxT("a"), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT );
-	autoFlexSizer->Add( m_button10, 0, wxALL, 5 );
+    m_ScrollWindow = new wxScrolledWindow( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxHSCROLL|wxVSCROLL );
+    m_ScrollWindow->SetScrollRate( 5, 5 );
+    wxBoxSizer* scrollSizer;
+    scrollSizer = new wxBoxSizer( wxVERTICAL );
 
-	m_button11 = new wxButton( this, wxID_ANY, wxT("b"), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT );
-	autoFlexSizer->Add( m_button11, 0, wxALL, 5 );
-	
-	m_button12 = new wxButton( this, wxID_ANY, wxT("c"), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT );
-	autoFlexSizer->Add( m_button12, 0, wxALL, 5 );
-	
-	m_button13 = new wxButton( this, wxID_ANY, wxT("dddddd"), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT );
-	autoFlexSizer->Add( m_button13, 0, wxALL, 5 );
-	
-	m_button14 = new wxButton( this, wxID_ANY, wxT("e"), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT );
-	autoFlexSizer->Add( m_button14, 0, wxALL, 5 );
-	
-	m_button15 = new wxButton( this, wxID_ANY, wxT("f"), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT );
-	autoFlexSizer->Add( m_button15, 0, wxALL, 5 );
-	
-	m_button16 = new wxButton( this, wxID_ANY, wxT("g"), wxDefaultPosition, wxDefaultSize, wxBU_EXACTFIT );
-	autoFlexSizer->Add( m_button16, 0, wxALL, 5 );
-	
-#endif
+    m_StaticText = new wxStaticText( m_ScrollWindow, wxID_ANY, wxT("+ Click for Properties"), wxDefaultPosition, wxDefaultSize, 0 );
+    m_StaticText->Wrap( -1 );
+    scrollSizer->Add( m_StaticText, 0, wxALL, 0 );
 
-	mainSizer->Add( autoFlexSizer, 0, wxEXPAND, 5 );
-	
-	m_Divider = new wxStaticLine( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLI_HORIZONTAL );
-	mainSizer->Add( m_Divider, 0, wxBOTTOM|wxEXPAND|wxTOP, 5 );
+    m_ScrollWindow->SetSizer( scrollSizer );
+    m_ScrollWindow->Layout();
+    scrollSizer->Fit( m_ScrollWindow );
+    bottomSizer->Add( m_ScrollWindow, 1, wxEXPAND | wxALL, 0 );
 
-	wxBoxSizer* bottomSizer;
-	bottomSizer = new wxBoxSizer( wxVERTICAL );
-	
-	m_ScrollWindow = new wxScrolledWindow( this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxHSCROLL|wxVSCROLL );
-	m_ScrollWindow->SetScrollRate( 5, 5 );
-	wxBoxSizer* scrollSizer;
-	scrollSizer = new wxBoxSizer( wxVERTICAL );
-	
-	m_StaticText = new wxStaticText( m_ScrollWindow, wxID_ANY, wxT("+ Click for Properties"), wxDefaultPosition, wxDefaultSize, 0 );
-	m_StaticText->Wrap( -1 );
-	scrollSizer->Add( m_StaticText, 0, wxALL, 0 );
-	
-	m_ScrollWindow->SetSizer( scrollSizer );
-	m_ScrollWindow->Layout();
-	scrollSizer->Fit( m_ScrollWindow );
-	bottomSizer->Add( m_ScrollWindow, 1, wxEXPAND | wxALL, 0 );
-	
-	mainSizer->Add( bottomSizer, 1, wxEXPAND, 5 );
-	
-	SetSizer( mainSizer );
-	Layout();
+    mainSizer->Add( bottomSizer, 1, wxEXPAND, 5 );
+
+    SetSizer( mainSizer );
+    Layout();
 }
 
 ToolsPanel::~ToolsPanel()
--- Luna/Scene/ToolsPanel.h
+++ Luna/Scene/ToolsPanel.h
@@ -2,25 +2,18 @@
 
 namespace Luna
 {
-  class SceneEditor;
+    class SceneEditor;
 
-  class ToolsPanel : public wxPanel 
-  {
-	  public:
-		  ToolsPanel( SceneEditor* sceneEditor, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 140, 300 ), long style = wxTAB_TRAVERSAL );
-		  virtual ~ToolsPanel();
-  	
-	  private:
-      SceneEditor* m_SceneEditor;
-		  wxButton* m_button10;
-		  wxButton* m_button11;
-		  wxButton* m_button12;
-		  wxButton* m_button13;
-		  wxButton* m_button14;
-		  wxButton* m_button15;
-		  wxButton* m_button16;
-		  wxStaticLine* m_Divider;
-		  wxScrolledWindow* m_ScrollWindow;
-		  wxStaticText* m_StaticText;  	
-  };
+    class ToolsPanel : public wxPanel 
+    {
+    public:
+        ToolsPanel( SceneEditor* sceneEditor, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 140, 300 ), long style = wxTAB_TRAVERSAL );
+        virtual ~ToolsPanel();
+
+    private:
+        SceneEditor* m_SceneEditor;
+        wxStaticLine* m_Divider;
+        wxScrolledWindow* m_ScrollWindow;
+        wxStaticText* m_StaticText;  	
+    };
 }

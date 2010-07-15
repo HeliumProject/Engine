dfb03cb0e55261f9b323d45df93369b4f51f9d88
Geoff evans <geoff@flummoxed.org>
2010-07-14 19:29:15 -0700

    add update script

---
+++ update_wx.pl
@@ -0,0 +1,25 @@
+use strict;
+use File::Basename;
+
+if ( !defined $ENV{wxWidgets} )
+{
+  die "wxWidgets is not defined in your environment!";
+}
+
+if ( !-d $ENV{wxWidgets} )
+{
+  die "wxWidgets environment variable does not point to a valid folder";
+}
+
+print( "\n o Changing CWD to $ENV{wxWidgets}\n");
+chdir $ENV{wxWidgets};
+
+print( "\n o Doing: svn update\n");
+system("svn update");
+
+my $origin = dirname $0;
+print( "\n o Changing $origin\n");
+chdir $origin;
+
+print( "\n o Doing: build_wx\n");
+do "build_wx.pl"

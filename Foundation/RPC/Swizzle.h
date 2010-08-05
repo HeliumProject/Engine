01e6bcc28705fc95c9143b0e3775bd8dc2fdfda9
Geoff Evans <geoff@flummoxed.org>
2010-08-05 11:38:05 -0700

    RPC

---
+++ Foundation/RPC/Swizzle.h
@@ -0,0 +1,100 @@
+#pragma once
+
+#include "Foundation/Memory/Endian.h"
+
+namespace Helium
+{
+    namespace RPC
+    {
+        typedef void (*SwizzleFunc)(void* data);
+
+        template <class T>
+        inline void Swizzle(T* data)
+        {
+#ifdef WIN32
+            HELIUM_BREAK();
+#endif
+        }
+
+        template<>
+        inline void Swizzle<u8>(u8* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+        template<> inline void Swizzle(i8* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+
+        template<> inline void Swizzle(u16* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+        template<> inline void Swizzle(i16* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+
+        template<> inline void Swizzle(u32* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+        template<> inline void Swizzle(i32* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+
+        template<> inline void Swizzle(u64* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+        template<> inline void Swizzle(i64* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+
+        template<> inline void Swizzle(f32* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+        template<> inline void Swizzle(f64* data)
+        {
+#ifdef WIN32
+            Helium::Swizzle(*data, true);
+#endif
+        }
+
+        template <class T>
+        inline void Swizzle(T& data)
+        {
+#ifdef WIN32
+            Swizzle(&data);
+#endif
+        }
+
+        template <class T>
+        SwizzleFunc GetSwizzleFunc()
+        {
+            void (*func)(T*) = &Swizzle<T>;
+            return (SwizzleFunc)(void*)func;
+        }
+    }
+}
---
+++ Foundation/RPC/Test.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "rpc.h"
+
+namespace Helium
+{
+    namespace RPC
+    {
+        namespace Test
+        {
+            struct TestArgs : RPC::Args
+            {
+                u8 m_char;
+                u32 m_integer;
+            };
+            typedef Helium::Signature<void, TestArgs&>::Delegate TestDelegate;
+
+            class TestInterface : public RPC::Interface
+            {
+            public:
+                TestInterface();
+
+                void Test( TestArgs& args );
+            };
+
+            void AddInterface(RPC::Host& host);
+        };
+    }
+}

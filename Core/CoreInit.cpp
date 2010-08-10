daffc181fb924cc9a76f3464c17c797f195b6f3e
Geoff Evans <geoff@flummoxed.org>
2010-08-10 11:55:51 -0700

    init content and asset in core

--- Core/CoreInit.cpp
+++ Core/CoreInit.cpp
@@ -1,4 +1,6 @@
 #include "CoreInit.h"
+#include "Content/ContentInit.h"
+#include "Asset/AssetInit.h"
 #include "Project.h"
 
 #include "Foundation/InitializerStack.h"
@@ -13,6 +15,8 @@ void Core::Initialize()
 {
     if ( ++g_InitCount == 1 )
     {
+        g_InitStack.Push( &Content::Initialize, &Content::Cleanup );
+        g_InitStack.Push( &Asset::Initialize, &Asset::Cleanup );
         g_InitStack.Push( Reflect::RegisterClass<Core::ProjectFile>( TXT("ProjectFile") ) );
         g_InitStack.Push( Reflect::RegisterClass<Core::ProjectFolder>( TXT("ProjectFolder") ) );
         g_InitStack.Push( Reflect::RegisterClass<Core::Project>( TXT("Project") ) );

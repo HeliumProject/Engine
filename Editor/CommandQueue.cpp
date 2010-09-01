aa6c47826f4810e9a4af56aeb1bca0bfafdd0806
Geoff Evans <geoff@flummoxed.org>
2010-08-31 17:53:29 -0700

    split Editor::CommandQueue into separate Foundation/Editor classes for re-use in the Core

--- Editor/CommandQueue.cpp
+++ Editor/CommandQueue.cpp
@@ -1,69 +1,28 @@
 #include "Precompile.h"
 #include "CommandQueue.h"
 
-#include "Platform/Mutex.h"
-
 using namespace Helium;
 using namespace Helium::Editor;
 
 DEFINE_EVENT_TYPE(wxEVT_FLUSH_COMMANDS)
 
-static Helium::Mutex g_CommandQueueMutex;
-
-CommandQueue::CommandQueue( wxWindow* window )
+Editor::CommandQueue::CommandQueue( wxWindow* window )
 : m_Window( window )
 {
     m_Window->Connect( m_Window->GetId(), wxEVT_FLUSH_COMMANDS, wxCommandEventHandler(CommandQueue::HandleEvent), NULL, this );
 }
 
-CommandQueue::~CommandQueue()
+Editor::CommandQueue::~CommandQueue()
 {
     m_Window->Disconnect( m_Window->GetId(), wxEVT_FLUSH_COMMANDS, wxCommandEventHandler(CommandQueue::HandleEvent), NULL, this );
 }
 
-void CommandQueue::Post( VoidSignature::Delegate delegate )
+void Editor::CommandQueue::EnqueueFlush()
 {
-    bool flush;
-
-    {
-        Helium::TakeMutex taken( g_CommandQueueMutex );
-
-        // the flash flag groups commands into a batch
-        //  - the first push on an empty queue schedules a flush via message
-        //  - subsequent pushes do not dispatch another message
-        //  - ownership of the messages is taken in a lock, so the all the messages queued during a message pump will be flushed together
-        //  - after the queue is emptied the next push will schedule the next message
-        flush = m_Commands.empty();
-
-        m_Commands.push_back( delegate );
-    }
-
-    if ( flush )
-    {
-        m_Window->GetEventHandler()->AddPendingEvent( wxCommandEvent ( wxEVT_FLUSH_COMMANDS, m_Window->GetId() ) );
-    }
+    m_Window->GetEventHandler()->AddPendingEvent( wxCommandEvent ( wxEVT_FLUSH_COMMANDS, m_Window->GetId() ) );
 }
 
-void CommandQueue::Flush()
-{
-    std::vector< VoidSignature::Delegate > commands;
-
-    {
-        Helium::TakeMutex taken ( g_CommandQueueMutex);
-        commands = m_Commands;
-        m_Commands.clear();
-    }
-
-    std::vector< VoidSignature::Delegate >::const_iterator itr = commands.begin();
-    std::vector< VoidSignature::Delegate >::const_iterator end = commands.end();
-    for ( ; itr != end; ++itr )
-    {
-        // perform the work
-        (*itr).Invoke( Helium::Void () );
-    }
-}
-
-void CommandQueue::HandleEvent( wxCommandEvent& event )
+void Editor::CommandQueue::HandleEvent( wxCommandEvent& event )
 {
     Flush();
 }
--- Editor/CommandQueue.h
+++ Editor/CommandQueue.h
@@ -2,49 +2,26 @@
 
 #include "Editor/API.h"
 
-#include "Foundation/Automation/Event.h"
-
-#include "Foundation/Undo/Command.h"
+#include "Foundation/CommandQueue.h"
 
 namespace Helium
 {
     namespace Editor
     {
-        //
-        // Designed to be aggregated into an editor, this class
-        //  allows deferred command execution to allow performing
-        //  commands to coexist easier with issuing commands in callbacks
-        //
-
-        struct PushCommandArgs
-        {
-            const Undo::V_CommandSmartPtr m_Commands;
-
-            PushCommandArgs( const Undo::V_CommandSmartPtr& commands )
-                : m_Commands (commands)
-            {
-
-            }
-        };
-
-        typedef Helium::Signature<void, const PushCommandArgs&> PushCommandSignature;
-
-        class CommandQueue : public wxEvtHandler
+        class CommandQueue : public Helium::CommandQueue, public wxEvtHandler
         {
         public:
             CommandQueue( wxWindow* window );
             ~CommandQueue();
 
         public:
-            void Post( VoidSignature::Delegate delegate );
-            void Flush();
+            void EnqueueFlush();
 
         protected:
             void HandleEvent( wxCommandEvent& event );
 
         private:
             wxWindow*                               m_Window;   // the windows to use (for custom message dispatch)
-            std::vector< VoidSignature::Delegate >  m_Commands; // the commands to redo when flushing
         };
     }
 }

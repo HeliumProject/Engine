7b86f7bab8e6107560a4138f0718189ee95ab789
Paul Haile <paulh@whitemoondreams.com>
2010-09-07 16:02:06 -0700

    added some new options to update.pl to make it a little more friendly.
    Update.pl - Update your stuff
    
    Options:
            -clean: passes -clean to each build step.
            -git_clean: cleans your git workspace
              -NOTE: This deletes all files not under revision control.
            -verbose: passes -verbose to each build step.
            -config <config name>: override default build config.

--- update.pl
+++ update.pl
@@ -5,25 +5,68 @@ use File::Basename;
 use Getopt::Long;
 
 # flags
-my $help = 0;
+my $help      = 0;
+my $clean     = 0;
+my $git_clean = 0;
+my $verbose   = 0;
+my $config    = '';
 
 # result
 my $result = 0;
 
-if ( !GetOptions( "h"         => \$help,
-                  "help"      => \$help,
-                  "usage"     => \$help ) || $help )
+if ( !GetOptions( "clean"      => \$clean,
+                  "git_clean"  => \$git_clean,
+                  "verbose"    => \$verbose,
+                  "c|config=s" => \$config,
+                  "h|help"     => \$help,
+                  "usage"      => \$help ) || $help )
 {
   print( "\nUpdate.pl - Update your stuff\n" );
   print( "\nOptions:\n" );
+  print( "\t-clean: passes -clean to each build step.\n" );
+  print( "\t-git_clean: cleans your git workspace \n\t  -NOTE: This deletes all files not under revision control.\n" );
+  print( "\t-verbose: passes -verbose to each build step.\n" );
+  print( "\t-config <config name>: override default build config.\n" );
   exit( 1 );
 }
 
-$result += _Do("git pull", "Git Pull");
+my $git_command;
+if ( $git_clean )
+{
+  my $git_command = 'git clean -fdx';
+  $result = _Do( $git_command, "Git Clean" );
+}
+$git_command = 'git pull';
+$result += _Do( $git_command, "Git Pull" );
 
-$result += _Do("perl.exe " . File::Spec->catfile( dirname( $0 ), "build.pl Dependencies.sln" ), "Build Dependencies");
+if ( $result )
+{
+  print("\nError during Git commands - Aborting script.\n");
+  exit( 1 );
+}
 
-$result += _Do("perl.exe " . File::Spec->catfile( dirname( $0 ), "build.pl Helium.sln" ), "Build Helium");
+
+my $dep_command    = 'perl.exe ' . File::Spec->catfile( dirname( $0 ), "build.pl Dependencies.sln" );
+my $helium_command = 'perl.exe ' . File::Spec->catfile( dirname( $0 ), "build.pl Helium.sln" );
+
+if($config)
+{
+  $dep_command    .= " -config \"$config\"";
+  $helium_command .= " -config \"$config\"";
+}
+if($clean)
+{
+  $dep_command    .= ' -clean';
+  $helium_command .= ' -clean';
+}
+if($verbose)
+{
+  $dep_command    .= ' -verbose';
+  $helium_command .= ' -verbose';
+}
+
+$result += _Do( $dep_command, "Build Dependencies" );
+$result += _Do( $helium_command, "Build Helium" );
 
 if ( $result )
 {

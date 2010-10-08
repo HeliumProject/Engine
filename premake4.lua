350c4fff59d2b4ab8209c39acd59442fada1c4be
Geoff Evans <geoff@flummoxed.org>
2010-10-08 15:45:52 -0700

    more premake progress... some of platform compiling

--- premake4.lua
+++ premake4.lua
@@ -1,18 +1,55 @@
-solution "Helium"
-	configurations { "Debug", "Debug Unicode", "Release", "Release Unicode" }
+-- notes:
+--  * keep in mind that solution-wide settings should be defined
+--    before ever mentioning configuratin-specific settings because
+--    the indentation in this file is for humans (not premake), and
+--    premake might infer conditionals when walking the graph of 
+--    solutions/configurations/projects
 
+solution "Helium"
+
+	location "Premake"
+	
+    platforms
+    {
+		"native"
+	}
+
+	configurations
+	{
+		"Debug",
+		"Debug Unicode",
+		"Release",
+		"Release Unicode"
+	}
+
+	flags
+	{
+		"NoMinimalRebuild"
+	}
+	
+	includedirs
+	{
+		"."
+	}
+	
 	configuration "Debug"
 		targetdir "Bin/Debug"
+
 	configuration "Debug Unicode"
 		targetdir "Bin/Debug Unicode"
+
 	configuration "Release"
 		targetdir "Bin/Release"
+
 	configuration "Release Unicode"
 		targetdir "Bin/Release Unicode"
+		
+	configuration "windows"
+		defines
+		{
+			"WIN32=1",
+		}
     
-    platforms { "Native", "x32", "x64" }
-	location "Build"
-
 	project "Platform"
 		kind "SharedLib"
 		language "C++"
@@ -21,13 +58,27 @@ solution "Helium"
 		configuration "windows"
 			files{ "Platform/Windows/*.h", "Platform/Windows/*.cpp" }
 
-		configuration "Debug*"
-			defines { "_DEBUG", "DEBUG" }
-			flags { "Symbols" }
+		configuration "macosx"
+			files{ "Platform/POSIX/*.h", "Platform/POSIX/*.cpp" }
+			
+		configuration "linux"
+			files{ "Platform/POSIX/*.h", "Platform/POSIX/*.cpp" }
+			
+		configuration "bsd"
+			files{ "Platform/POSIX/*.h", "Platform/POSIX/*.cpp" }
 
-		configuration "Release*"
-			defines { "NDEBUG" }
-			flags { "Optimize" }
+	project "Foundation"
+		kind "SharedLib"
+		language "C++"
+		files { "Foundation/**.h", "Foundation/**.cpp" }
 
-		configuration "*Unicode"
-			defines { "UNICODE=1" }
+	configuration "Debug*"
+		defines { "_DEBUG", "DEBUG" }
+		flags { "Symbols" }
+
+	configuration "Release*"
+		defines { "NDEBUG" }
+		flags { "Optimize" }
+
+	configuration "*Unicode"
+		defines { "UNICODE=1" }

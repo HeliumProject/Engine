5215c14b1d175022d511a9069920415eb130b2cd
Geoff Evans <geoff@flummoxed.org>
2010-10-08 11:52:23 -0700

    premake experimentation

--- premake4.lua
+++ premake4.lua
@@ -1,12 +1,25 @@
 solution "Helium"
 	configurations { "Debug", "Debug Unicode", "Release", "Release Unicode" }
-	platforms { "Native", "x32", "x64" }
+
+	configuration "Debug"
+		targetdir "Bin/Debug"
+	configuration "Debug Unicode"
+		targetdir "Bin/Debug Unicode"
+	configuration "Release"
+		targetdir "Bin/Release"
+	configuration "Release Unicode"
+		targetdir "Bin/Release Unicode"
+    
+    platforms { "Native", "x32", "x64" }
 	location "Build"
 
 	project "Platform"
 		kind "SharedLib"
 		language "C++"
-		files { "Platform/**.h", "Platform/**.cpp" }
+		files { "Platform/*.h", "Platform/*.cpp" }
+		
+		configuration "windows"
+			files{ "Platform/Windows/*.h", "Platform/Windows/*.cpp" }
 
 		configuration "Debug*"
 			defines { "_DEBUG", "DEBUG" }

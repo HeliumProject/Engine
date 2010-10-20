d894e6f3b6bcc2548fb177e34b2281812610ea78
Geoff Evans <geoff@flummoxed.org>
2010-10-19 17:56:37 -0700

    update premake for external repos

--- premake4.lua
+++ premake4.lua
@@ -4,6 +4,12 @@ solution "Dependencies"
 Helium.DoDefaultSolutionSettings()
 dofile "Dependencies.lua"
 
+if _ACTION ~= "clean" then
+	Helium.BuildWxWidgets( "." )
+else
+	Helium.CleanWxWidgets( "." )
+end
+
 solution "Helium"
 Helium.DoDefaultSolutionSettings()
 dofile "Helium.lua"

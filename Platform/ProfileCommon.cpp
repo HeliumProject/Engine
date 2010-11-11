e69753493aad782ea122fecbd69d3c4d1280baf0
Geoff Evans <geoff@flummoxed.org>
2010-11-10 17:07:25 -0800

    oops, missing file

---
+++ Platform/ProfileCommon.cpp
@@ -0,0 +1,13 @@
+#include "Platform/Profile.h"
+
+using namespace Helium;
+
+void Timer::Reset()
+{
+    m_StartTime = Helium::TimerGetClock();
+}
+
+float Timer::Elapsed()
+{
+    return Helium::CyclesToMillis(Helium::TimerGetClock() - m_StartTime);
+}

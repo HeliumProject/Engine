26e2f7deb3790da81c93d43d188bb0bc96da92ca
Andy Burke <aburke@bitflood.org>
2010-11-10 18:51:44 -0800

    - rework some of how the directory stuff is handled

--- Platform/Windows/Stat.cpp
+++ Platform/Windows/Stat.cpp
@@ -11,21 +11,21 @@ bool Helium::StatPath( const tchar_t* path, Helium::Stat& stat )
     if ( result )
     {
         if ( ( windowsStats.st_mode & _S_IFREG ) == _S_IFREG )
-            stat.m_Mode |= ModeFlags::File;
+            stat.m_Mode |= FileModeFlags::File;
         if ( ( windowsStats.st_mode & _S_IFDIR ) == _S_IFDIR )
-            stat.m_Mode |= ModeFlags::Directory;
+            stat.m_Mode |= FileModeFlags::Directory;
         if ( ( windowsStats.st_mode & _S_IFIFO ) == _S_IFIFO )
-            stat.m_Mode |= ModeFlags::Pipe;
+            stat.m_Mode |= FileModeFlags::Pipe;
         if ( ( windowsStats.st_mode & _S_IFCHR ) == _S_IFCHR )
-            stat.m_Mode |= ModeFlags::Special;
+            stat.m_Mode |= FileModeFlags::Special;
 
 
         if ( ( windowsStats.st_mode & _S_IREAD ) == _S_IREAD )
-            stat.m_Mode |= ModeFlags::Read;
+            stat.m_Mode |= FileModeFlags::Read;
         if ( ( windowsStats.st_mode & _S_IWRITE ) == _S_IWRITE )
-            stat.m_Mode |= ModeFlags::Write;
+            stat.m_Mode |= FileModeFlags::Write;
         if ( ( windowsStats.st_mode & _S_IEXEC ) == _S_IEXEC )
-            stat.m_Mode |= ModeFlags::Execute;
+            stat.m_Mode |= FileModeFlags::Execute;
 
         stat.m_AccessTime = windowsStats.st_atime;
         stat.m_CreatedTime = windowsStats.st_ctime;

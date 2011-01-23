1efa05a4e77d6c4c6a4c66508af4780053c97bf0
Geoff Evans <geoff@flummoxed.org>
2011-01-22 17:18:02 -0800

    fix premake to not starting building prerequisites if the user is just asking for help or version information from premake

--- premake4.lua
+++ premake4.lua
@@ -2,67 +2,72 @@ require "Base"
 require "TBB"
 require "WxWidgets"
 
--- Check prereqs
-Helium.CheckEnvironment()
+-- Do nothing if there is no action (--help, etc...)
+if _ACTION then
 
--- Check for FBX integration.
-haveFbx = os.isfile( "Integrations/FBX/FBX.lua" )
-if haveFbx then
-	require "Integrations/Fbx/Fbx"
-end
+	-- Check prereqs
+	Helium.CheckEnvironment()
 
--- Check for Granny integration.
-haveGranny = os.isfile( "Integrations/Granny/Granny.lua" )
-if haveGranny then
-	require "Integrations/Granny/Granny"
-end
-
--- Custom action to just run the pre-build scripts.
-newaction
-{
-	trigger = "prebuild",
-	description = "Run the pre-build scripts without updating project or make files",
-	execute = Helium.Prebuild
-}
-
--- Custom option to set the global Unicode (wchar_t/UTF-16,32)) vs. ASCII (char/UTF-8)
-newoption
-{
-    trigger = "no-unicode",
-    description = "Disable Unicode support"
-}
-
-if _ACTION ~= "prebuild" then
-
-	if _ACTION ~= "clean" then
-        local bin = "Bin"
-
-		Helium.BuildWxWidgets()
-		Helium.PublishWxWidgets( bin )
-		Helium.BuildTBB()
-		Helium.PublishTBB( bin )
-
-		if haveFbx then
-			Helium.PublishFBX( bin )
-		end
-
-		if haveGranny then
-			Helium.PublishGranny( bin )
-		end
-
-        Helium.PublishIcons( bin )
-
-		Helium.Prebuild()
-	else
-		Helium.CleanWxWidgets()
-		Helium.CleanTBB()
+	-- Check for FBX integration.
+	haveFbx = os.isfile( "Integrations/FBX/FBX.lua" )
+	if haveFbx then
+		require "Integrations/Fbx/Fbx"
 	end
 
-	solution "Dependencies"
-	Helium.DoDefaultSolutionSettings()
-	dofile "Dependencies.lua"
+	-- Check for Granny integration.
+	haveGranny = os.isfile( "Integrations/Granny/Granny.lua" )
+	if haveGranny then
+		require "Integrations/Granny/Granny"
+	end
 
-	solution "Helium"
-	Helium.DoDefaultSolutionSettings()
-	dofile "Helium.lua"
+	-- Custom action to just run the pre-build scripts.
+	newaction
+	{
+		trigger = "prebuild",
+		description = "Run the pre-build scripts without updating project or make files",
+		execute = Helium.Prebuild
+	}
+
+	-- Custom option to set the global Unicode (wchar_t/UTF-16,32)) vs. ASCII (char/UTF-8)
+	newoption
+	{
+		trigger = "no-unicode",
+		description = "Disable Unicode support"
+	}
+
+	if _ACTION ~= "prebuild" then
+
+		if _ACTION ~= "clean" then
+			local bin = "Bin"
+
+			Helium.BuildWxWidgets()
+			Helium.PublishWxWidgets( bin )
+			Helium.BuildTBB()
+			Helium.PublishTBB( bin )
+
+			if haveFbx then
+				Helium.PublishFBX( bin )
+			end
+
+			if haveGranny then
+				Helium.PublishGranny( bin )
+			end
+
+			Helium.PublishIcons( bin )
+
+			Helium.Prebuild()
+		else
+			Helium.CleanWxWidgets()
+			Helium.CleanTBB()
+		end
+
+		solution "Dependencies"
+		Helium.DoDefaultSolutionSettings()
+		dofile "Dependencies.lua"
+
+		solution "Helium"
+		Helium.DoDefaultSolutionSettings()
+		dofile "Helium.lua"
+	end
+	
 end

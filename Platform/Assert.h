359d016d49226e4db70ae8601678319970d67f3a
Ted Cipicchio <tcipicchio@gmail.com>
2011-01-24 19:46:37 -0800

    Modified HELIUM_TRIGGER_ASSERT_HANDLER() to hopefully minimize clashes between local variable names.

--- Platform/Assert.h
+++ Platform/Assert.h
@@ -96,17 +96,17 @@ namespace Helium
 #define HELIUM_TRIGGER_ASSERT_HANDLER( EXP, ... ) \
     { \
         HELIUM_DISABLEABLE_CODE_BLOCK( \
-            Helium::Assert::EResult result = Helium::Assert::Trigger( \
+            Helium::Assert::EResult _result = Helium::Assert::Trigger( \
                 EXP, \
                 HELIUM_FUNCTION_NAME, \
                 __FILE__, \
                 __LINE__, \
                 __VA_ARGS__ ); \
-            if( result == Helium::Assert::RESULT_BREAK ) \
+            if( _result == Helium::Assert::RESULT_BREAK ) \
             { \
                 HELIUM_ISSUE_BREAK(); \
             } \
-            else if( result == Helium::Assert::RESULT_ABORT ) \
+            else if( _result == Helium::Assert::RESULT_ABORT ) \
             { \
                 Helium::FatalExit( -1 ); \
             } ) \

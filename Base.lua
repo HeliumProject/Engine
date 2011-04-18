0e9278dc7f53d10b913b079c62733ff60e2f208b
Geoff Evans <geoff@flummoxed.org>
2011-04-18 10:27:12 -0700

    fix python on mac

--- Base.lua
+++ Base.lua
@@ -219,21 +219,26 @@ end
 -- Pre-build script execution.
 Helium.Prebuild = function()
 
+	local python = "python"
+	
+	if os.get() == "windows" then
+		python = python .. ".exe"
+    else
+		python = python .. "3"
+    end
+
+	local pythonPath = os.pathsearch( python, os.getenv( 'PATH' ) )
+	if pythonPath == nil then
+		error( "\n\nYou must have Python 3.x installed and in your PATH to continue." )
+	end
+
 	local commands =
 	{
-		"python Build/JobDefParser.py JobDefinitions . .",
-		"python Build/TypeParser.py D3D9Rendering EditorSupport Engine EngineJobs Framework FrameworkWin Graphics GraphicsJobs GraphicsTypes PcSupport PreprocessingPc Rendering TestJobs WinWindowing Windowing",
-		"python Build/TypeParser.py -i Example -s Example -p EXAMPLE_ ExampleGame ExampleMain",
+		python .. " Build/JobDefParser.py JobDefinitions . .",
+		python .. " Build/TypeParser.py D3D9Rendering EditorSupport Engine EngineJobs Framework FrameworkWin Graphics GraphicsJobs GraphicsTypes PcSupport PreprocessingPc Rendering TestJobs WinWindowing Windowing",
+		python .. " Build/TypeParser.py -i Example -s Example -p EXAMPLE_ ExampleGame ExampleMain",
 	}
-    
-    local pythonPath = os.pathsearch( 'python', os.getenv( 'PATH' ) )
-    if pythonPath == nil then
-        pythonPath = os.pathsearch( 'python.exe', os.getenv( 'PATH' ) )
-        if pythonPath == nil then
-            error( "\n\nYou must have Python 3.x installed and in your PATH to continue." )
-        end
-    end
-        
+
 	local result = 0
 
 	for i, commandString in ipairs( commands ) do

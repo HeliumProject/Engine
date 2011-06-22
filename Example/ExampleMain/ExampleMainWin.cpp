fa048f98d4c39fe5e39b18f6c42697d851c40795
Philip Degarmo <aclysma@gmail.com>
2011-06-22 01:47:48 -0500

    Undoing a couple unnecessary changes brought in with previous merge

--- Example/ExampleMain/ExampleMainWin.cpp
+++ Example/ExampleMain/ExampleMainWin.cpp
@@ -9,42 +9,7 @@
 
 #include "ExampleMain/ObjectTypeRegistration.h"
 
-#include "Framework/Components.h"
-#include "Framework/Facets.h"
-
-#include "Foundation/Math/Vector2.h"
-
 using namespace Lunar;
-
-struct FLocated2 : public Helium::Facets::IFacet
-{
-  REFLECT_DECLARE_OBJECT(FLocated2, Helium::Facets::IFacet)
-
-  Helium::Vector2 m_Location;
-};
-
-REFLECT_DEFINE_OBJECT(FLocated2);
-
-class CTestComponent : public Helium::Components::Component
-{
-  OBJECT_DECLARE_COMPONENT(CTestComponent, Helium::Components::Component);
-};
-
-OBJECT_DEFINE_COMPONENT(CTestComponent);
-
-class OTestObject : public Reflect::Object, public Components::Host
-{
-public:
-  REFLECT_DECLARE_OBJECT(OTestObject, Reflect::Object);
-
-  FLocated2 m_Located2;
-
-  ENGINE_FACETS_BEGIN(OTestObject)
-    REGISTER_FACET(FLocated2, m_Located2)
-  ENGINE_FACETS_END()
-};
-
-REFLECT_DEFINE_OBJECT(OTestObject);
 
 /// Windows application entry point.
 ///
@@ -61,16 +26,7 @@ int APIENTRY _tWinMain( HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR
     int32_t result = 0;
 
     {
-		Path baseDirectory;
-		if ( !File::GetBaseDirectory( baseDirectory ) )
-		{
-			HELIUM_TRACE( TRACE_ERROR, TXT( "Could not get base directory." ) );
-			return -1;
-		}
-
-		HELIUM_VERIFY( CacheManager::InitializeStaticInstance( baseDirectory ) );
-
-		// Initialize a GameSystem instance.
+        // Initialize a GameSystem instance.
         CommandLineInitializationWin commandLineInitialization;
         Example::ObjectTypeRegistration< ObjectTypeRegistrationWin > objectTypeRegistration;
         MemoryHeapPreInitialization memoryHeapPreInitialization;
@@ -79,8 +35,6 @@ int APIENTRY _tWinMain( HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR
         WindowManagerInitializationWin windowManagerInitialization( hInstance, nCmdShow );
         RendererInitializationWin rendererInitialization;
         //NullRendererInitialization rendererInitialization;
-
-        
 
         GameSystem* pGameSystem = GameSystem::CreateStaticInstance();
         HELIUM_ASSERT( pGameSystem );
@@ -93,26 +47,6 @@ int APIENTRY _tWinMain( HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR
             windowManagerInitialization,
             rendererInitialization,
             NULL );
-
-
-
-        Components::Initialize();
-        Facets::Initialize();
-
-        Helium::Reflect::RegisterClassType<FLocated2>(TXT("FLocated2"));
-        Helium::Reflect::RegisterClassType<CTestComponent>(TXT("CTestComponent"));
-        CTestComponent::RegisterComponentType(16); // Up to 16 availabled
-        Helium::Reflect::RegisterClassType<OTestObject>(TXT("OTestObject"));
-        OTestObject::InitializeFacetOffsets();
-
-        OTestObject to;
-        CTestComponent *component1 = to.AttachComponent<CTestComponent>();
-        FLocated2 *facet1 = to.GetFacet<FLocated2>();
-
-
-        Components::Host host;
-
-
         if( bSystemInitSuccess )
         {
             // Run the application.

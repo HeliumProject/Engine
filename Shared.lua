50cef219e4de39978d6340b78ee5c953af1e91a4
Philip Degarmo <aclysma@gmail.com>
2011-09-22 23:15:44 -0500

    Add TestApp and TestJobs to Tools sln

--- Shared.lua
+++ Shared.lua
@@ -654,3 +654,112 @@ project( prefix .. "FrameworkWin" )
 			prefix .. "PreprocessingPc",
 			prefix .. "EditorSupport",
 		}
+		
+		
+project( prefix .. "TestJobs" )-- DEPRECATED
+	uuid "12106586-0EB1-4D4C-9DFE-E3C63D3E4013"
+
+	Helium.DoModuleProjectSettings( ".", "HELIUM", "TestJobs", "TEST_JOBS" )
+	
+	files
+	{
+		"TestJobs/**",
+	}
+
+	configuration "SharedLib"
+		links
+		{
+			prefix .. "Platform",
+			prefix .. "Foundation",
+			prefix .. "Engine",
+			prefix .. "EngineJobs",
+		}
+
+project( prefix .. "TestApp" )-- DEPRECATED
+	uuid "CB5427DC-CE08-4FA6-B060-F35A902806BA"
+
+	kind "WindowedApp"
+
+	files
+	{
+		"TestApp/**.cpp",
+		"TestApp/**.h",
+		"TestApp/**.ico",
+		"TestApp/**.rc"
+	}
+
+	flags
+	{
+		"WinMain",
+	}
+
+	links
+	{
+		prefix .. "Platform",
+		prefix .. "Math",
+		prefix .. "Foundation",
+		prefix .. "Engine",
+		prefix .. "EngineJobs",
+		prefix .. "Windowing",
+		prefix .. "Rendering",
+		prefix .. "GraphicsTypes",
+		prefix .. "GraphicsJobs",
+		prefix .. "Graphics",
+		prefix .. "Framework",
+		prefix .. "WindowingWin",
+		prefix .. "RenderingD3D9",
+		prefix .. "PcSupport",
+		prefix .. "PreprocessingPc",
+		prefix .. "EditorSupport",
+		prefix .. "FrameworkWin",
+		prefix .. "TestJobs",
+	}
+
+	pchheader( "TestAppPch.h" )
+	pchsource( "TestApp/TestAppPch.cpp" )
+
+	Helium.DoDefaultProjectSettings()
+
+	-- TestApp is a bit odd because it includes custom game objects and a main().
+	-- So we need the dll export #defines. But calling DoModuleProjectSettings(...) above
+	-- seems to blow away the libs we try to import when we call DoDefaultProjectSettings()
+	configuration { "windows", "Debug" }
+	defines
+	{
+		"HELIUM_TEST_APP_EXPORTS",
+	}
+
+	configuration "windows"
+		links
+		{
+			"d3d9",
+			"d3dx9",
+			"d3d11",
+			"dxguid",
+			"d3dcompiler",
+			"wininet",
+			"ws2_32",
+			"dbghelp",
+		}
+	configuration { "windows", "x32" }
+		links
+		{
+			"fbxsdk_20113_1",
+		}
+	configuration { "windows", "x64" }
+		links
+		{
+			"fbxsdk_20113_1_amd64",
+		}
+	if haveGranny then
+		configuration "x32"
+			links
+			{
+				"granny2",
+			}
+		configuration "x64"
+			links
+			{
+				"granny2_x64",
+			}
+	end

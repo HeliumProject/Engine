25a1c0b40d89dd228d87c53e3a30f276fad604a6
Geoff Evans <geoff@flummoxed.org>
2011-11-23 22:16:47 -0800

    working premake execution on macosx 10.7.2

--- FBX.lua
+++ FBX.lua
@@ -18,10 +18,10 @@ Helium.PublishFBX = function( bin )
 			table.insert( files, { file="fbxsdk-2012.2.dll",  source=Helium.GetFbxSdkLocation() .. "/lib/" .. _ACTION .. "/x64", target=bin .. "/x64/Release" } )
 		end
 	elseif os.get() == "macosx" then
-		table.insert( files, { file="libfbxsdk-2012.2d.dylib", source=Helium.GetFbxSdkLocation() .. "/lib/gcc4", target=bin .. "/x32/Debug" } )
-		table.insert( files, { file="libfbxsdk-2012.2.dylib",  source=Helium.GetFbxSdkLocation() .. "/lib/gcc4", target=bin .. "/x32/Intermediate" } )
-		table.insert( files, { file="libfbxsdk-2012.2.dylib",  source=Helium.GetFbxSdkLocation() .. "/lib/gcc4", target=bin .. "/x32/Profile" } )
-		table.insert( files, { file="libfbxsdk-2012.2.dylib",  source=Helium.GetFbxSdkLocation() .. "/lib/gcc4", target=bin .. "/x32/Release" }	)
+		table.insert( files, { file="libfbxsdk-2012.2d.dylib", source=Helium.GetFbxSdkLocation() .. "/lib/gcc4/ub", target=bin .. "/x32/Debug" } )
+		table.insert( files, { file="libfbxsdk-2012.2.dylib",  source=Helium.GetFbxSdkLocation() .. "/lib/gcc4/ub", target=bin .. "/x32/Intermediate" } )
+		table.insert( files, { file="libfbxsdk-2012.2.dylib",  source=Helium.GetFbxSdkLocation() .. "/lib/gcc4/ub", target=bin .. "/x32/Profile" } )
+		table.insert( files, { file="libfbxsdk-2012.2.dylib",  source=Helium.GetFbxSdkLocation() .. "/lib/gcc4/ub", target=bin .. "/x32/Release" }	)
 	else
 		print("Implement support for " .. os.get() .. " to PublishFBX()")
 		os.exit(1)

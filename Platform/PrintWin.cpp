e911494755d8e6b6a69b07a55b5ef6f7d0d315d0
Geoff Evans <geoff@flummoxed.org>
2012-11-01 13:29:48 -0700

    fix print functions

--- Platform/PrintWin.cpp
+++ Platform/PrintWin.cpp
@@ -10,40 +10,64 @@ int Helium::Print(const tchar_t* fmt, ...)
 {
 	va_list args;
 	va_start(args, fmt);
-	int result = _vtprintf(fmt, args);
+#if HELIUM_WCHAR_T
+	int result = vwprintf(fmt, args);
+#else
+	int result = vprintf(fmt, args);
+#endif
 	va_end(args);
 	return result;
 }
 
 int Helium::PrintArgs(const tchar_t* fmt, va_list args)
 {
-	return _vtprintf(fmt, args);
+#if HELIUM_WCHAR_T
+	return vwprintf(fmt, args);
+#else
+	return vprintf(fmt, args);
+#endif
 }
 
 int Helium::FilePrint(FILE* f, const tchar_t* fmt, ...)
 {
 	va_list args;
 	va_start(args, fmt);
-	int result = _vftprintf(f, fmt, args);
+#if HELIUM_WCHAR_T
+	int result = vfwprintf(f, fmt, args);
+#else
+	int result = vfprintf(f, fmt, args);
+#endif
 	va_end(args);
 	return result;
 }
 
 int Helium::FilePrintArgs(FILE* f, const tchar_t* fmt, va_list args)
 {
-	return _vftprintf(f, fmt, args);
+#if HELIUM_WCHAR_T
+	return vfwprintf(f, fmt, args);
+#else
+	return vfprintf(f, fmt, args);
+#endif
 }
 
 int Helium::StringPrint(tchar_t* dest, size_t destCount, const tchar_t* fmt, ...)
 {
 	va_list args;
 	va_start(args, fmt);
-	int result = _vsntprintf(dest, destCount, fmt, args);
+#if HELIUM_WCHAR_T
+	int result = vsnwprintf(dest, destCount, fmt, args);
+#else
+	int result = vsnprintf(dest, destCount, fmt, args);
+#endif
 	va_end(args);
 	return result;
 }
 
 int Helium::StringPrintArgs(tchar_t* dest, size_t destCount, const tchar_t* fmt, va_list args)
 {
-	return _vsntprintf(dest, destCount, fmt, args);
+#if HELIUM_WCHAR_T
+	return vsnwprintf(dest, destCount, fmt, args);
+#else
+	return vsnprintf(dest, destCount, fmt, args);
+#endif
 }

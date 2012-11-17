ecc65664a79fe627e96dcbba1930ffd91baad116
Geoff Evans <geoff@flummoxed.org>
2012-11-16 19:05:47 -0800

    move ArrayIterator classes into array source file

--- Foundation/DynArray.h
+++ Foundation/DynArray.h
@@ -6,7 +6,6 @@
 
 #include "Foundation/API.h"
 #include "Foundation/Math.h"
-#include "Foundation/ArrayIterator.h"
 
 #include "boost/preprocessor/repetition/enum_params.hpp"
 #include "boost/preprocessor/repetition/enum_binary_params.hpp"
@@ -14,7 +13,102 @@
 
 namespace Helium
 {
-    /// Resizable array (not thread-safe).
+    /// Constant general-purpose array iterator.
+    template< typename T >
+    class ConstArrayIterator
+    {
+    public:
+        /// Type for iterator values.
+        typedef T ValueType;
+
+        /// Type for pointers to iterated elements.
+        typedef T* PointerType;
+        /// Type for references to iterated elements.
+        typedef T& ReferenceType;
+        /// Type for constant pointers to iterated elements.
+        typedef const T* ConstPointerType;
+        /// Type for constant references to iterated elements.
+        typedef const T& ConstReferenceType;
+
+        /// @name Construction/Destruction
+        //@{
+        ConstArrayIterator();
+        explicit ConstArrayIterator( const T* pElement );
+        //@}
+
+        /// @name Overloaded Operators
+        //@{
+        ConstReferenceType operator*() const;
+        ConstPointerType operator->() const;
+
+        ConstArrayIterator& operator++();
+        ConstArrayIterator operator++( int );
+        ConstArrayIterator& operator--();
+        ConstArrayIterator operator--( int );
+
+        ConstArrayIterator& operator+=( ptrdiff_t offset );
+        ConstArrayIterator operator+( ptrdiff_t offset ) const;
+        ConstArrayIterator& operator-=( ptrdiff_t offset );
+        ConstArrayIterator operator-( ptrdiff_t offset ) const;
+
+        ptrdiff_t operator-( const ConstArrayIterator& rOther ) const;
+
+        bool operator==( const ConstArrayIterator& rOther ) const;
+        bool operator!=( const ConstArrayIterator& rOther ) const;
+        bool operator<( const ConstArrayIterator& rOther ) const;
+        bool operator>( const ConstArrayIterator& rOther ) const;
+        bool operator<=( const ConstArrayIterator& rOther ) const;
+        bool operator>=( const ConstArrayIterator& rOther ) const;
+        //@}
+
+    protected:
+        /// Current array element.
+        PointerType m_pElement;
+    };
+
+    /// Non-constant general-purpose array iterator.
+    template< typename T >
+    class ArrayIterator : public ConstArrayIterator< T >
+    {
+    public:
+        /// Type for iterator values.
+        typedef T ValueType;
+
+        /// Type for pointers to iterated elements.
+        typedef T* PointerType;
+        /// Type for references to iterated elements.
+        typedef T& ReferenceType;
+        /// Type for constant pointers to iterated elements.
+        typedef const T* ConstPointerType;
+        /// Type for constant references to iterated elements.
+        typedef const T& ConstReferenceType;
+
+        /// @name Construction/Destruction
+        //@{
+        ArrayIterator();
+        explicit ArrayIterator( T* pElement );
+        //@}
+
+        /// @name Overloaded Operators
+        //@{
+        ReferenceType operator*() const;
+        PointerType operator->() const;
+
+        ArrayIterator& operator++();
+        ArrayIterator operator++( int );
+        ArrayIterator& operator--();
+        ArrayIterator operator--( int );
+
+        ArrayIterator& operator+=( ptrdiff_t offset );
+        ArrayIterator operator+( ptrdiff_t offset ) const;
+        ArrayIterator& operator-=( ptrdiff_t offset );
+        ArrayIterator operator-( ptrdiff_t offset ) const;
+
+        ptrdiff_t operator-( const ConstArrayIterator& rOther ) const;
+        //@}
+    };
+	
+	/// Resizable array (not thread-safe).
     template< typename T, typename Allocator = DefaultAllocator >
     class DynArray
     {

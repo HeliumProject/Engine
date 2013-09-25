30201ce46e57fdb42e41377e32fc246ee049aebf
Geoff Evans <geoff@flummoxed.org>
2013-09-25 15:27:44 -0700

    update install-packages.sh: add boost (for regex) on linux

--- Dependencies/install-packages.sh
+++ Dependencies/install-packages.sh
@@ -1,1 +1,1 @@
-sudo apt-get install build-essential libgl1-mesa-dev libglu1-mesa-dev libgtk2.0-dev
+sudo apt-get install build-essential libboost-all-dev libgl1-mesa-dev libglu1-mesa-dev libgtk2.0-dev

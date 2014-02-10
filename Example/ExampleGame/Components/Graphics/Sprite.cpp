fab6ef3cd976460255015ba0cae444c52a95b64b
Geoff Evans <geoff@flummoxed.org>
2014-02-09 16:31:30 -0800

    add sprite for player shot

--- Example/ExampleGame/Components/Graphics/Sprite.cpp
+++ Example/ExampleGame/Components/Graphics/Sprite.cpp
@@ -95,12 +95,11 @@ void ExampleGame::SpriteComponentDefinition::PopulateMetaType( Helium::Reflect::
 {
 	comp.AddField( &SpriteComponentDefinition::m_Rotation, "m_Rotation" );
 	comp.AddField( &SpriteComponentDefinition::m_Scale, "m_Scale" );
-	comp.AddField( &SpriteComponentDefinition::m_Texture, "m_Texture" );
 	comp.AddField( &SpriteComponentDefinition::m_TopLeftPixel, "m_TopLeftPixel" );
 	comp.AddField( &SpriteComponentDefinition::m_FrameSize, "m_FrameSize" );
 	comp.AddField( &SpriteComponentDefinition::m_FramesPerColumn, "m_FramesPerColumn" );
 	comp.AddField( &SpriteComponentDefinition::m_FrameCount, "m_FrameCount" );
-	comp.AddField( &SpriteComponentDefinition::m_Texture, "m_Texture" );
+	comp.AddField( &SpriteComponentDefinition::m_Texture, "m_Texture", Reflect::FieldFlags::Share );
 }
 
 Helium::Point ExampleGame::SpriteComponentDefinition::GetPixelCoordinates( uint32_t frame ) const

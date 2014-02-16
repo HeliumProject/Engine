458d5d390eb2eb27df2320880badb04c85312edc
Matt Stine <stine.matt@gmail.com>
2014-02-15 22:06:38 -0800

    Implement GLImmediateCommandProxy::SetRenderSurfaces.

--- RenderingGL/GLImmediateCommandProxy.cpp
+++ RenderingGL/GLImmediateCommandProxy.cpp
@@ -53,11 +53,20 @@ void GLImmediateCommandProxy::SetRenderSurfaces( RSurface* pRenderTargetSurface,
 {
 	HELIUM_ASSERT( pRenderTargetSurface );
 
-	GLFWwindow* pGlfwWindow = static_cast< GLSurface* >( pRenderTargetSurface )->GetGLSurface();
-	HELIUM_ASSERT( pGlfwWindow );
+	GLuint colorRenderbuffer = static_cast< GLSurface* >( pRenderTargetSurface )->GetGLSurface();
+	HELIUM_ASSERT( colorRenderbuffer != 0 );
+	glFramebufferRenderbuffer( GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorRenderbuffer );
 
-	// TODO: naive implementation for now assumes always drawing to back buffer.
-	glfwMakeContextCurrent( pGlfwWindow );
+	GLuint depthStencilRenderbuffer = static_cast< GLSurface* >( pDepthStencilSurface )->GetGLSurface();
+	HELIUM_ASSERT( depthStencilRenderbuffer != 0 );
+	glFramebufferRenderbuffer( GL_DRAW_FRAMEBUFFER, GL_DEPTH_STENCIL, GL_RENDERBUFFER, depthStencilRenderbuffer );
+
+	GLenum framebufferStatus = glCheckFramebufferStatus( GL_DRAW_FRAMEBUFFER );
+	HELIUM_ASSERT( framebufferStatus == GL_FRAMEBUFFER_COMPLETE );
+	if( framebufferStatus != GL_FRAMEBUFFER_COMPLETE )
+	{
+		HELIUM_TRACE( TraceLevels::Error, "GLImmediateCommandProxy: Incomplete framebuffer object created." );
+	}
 }
 
 /// @copydoc RRenderCommandProxy::SetViewport()

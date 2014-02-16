62e2347f19737d34d524498ead7248c3a958407c
Matt Stine <stine.matt@gmail.com>
2014-02-15 22:05:14 -0800

    Fix spacing and indentation.

--- Windowing/WindowManagerGLFW.cpp
+++ Windowing/WindowManagerGLFW.cpp
@@ -35,10 +35,10 @@ bool WindowManager::Initialize( )
 	m_isInitialized = (GL_TRUE == glfwInit());
 	HELIUM_ASSERT( m_isInitialized );
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+	glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 2 );
+	glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
+	glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 
 	return m_isInitialized;
 }

1a18e0477ca00961f9192a838ae2888ac50fd02b
Matt Stine <stine.matt@gmail.com>
2014-02-15 22:04:16 -0800

    Create new renderbuffer upon back buffer request in GLMainContext.

--- RenderingGL/GLMainContext.cpp
+++ RenderingGL/GLMainContext.cpp
@@ -27,7 +27,31 @@ RSurface* GLMainContext::GetBackBufferSurface()
 	// Create the back buffer surface reference if it does not yet exist.
 	if( !m_spBackBufferSurface )
 	{
-		m_spBackBufferSurface = new GLSurface( m_pGlfwWindow );
+		GLuint newRenderbuffer = 0;
+		glGenRenderbuffers( 1, &newRenderbuffer );
+		HELIUM_ASSERT( newRenderbuffer != 0 );
+		if( newRenderbuffer == 0 )
+		{
+			HELIUM_TRACE( TraceLevels::Error, "GLMainContext: Failed to generate GL back renderbuffer surface.\n" );
+		}
+
+		// Store off previously bound renderbuffer.
+		GLint curRenderbuffer = 0;
+		glGetIntegerv( GL_RENDERBUFFER_BINDING, &curRenderbuffer );
+
+		// Create a new renderbuffer object for the back buffer.
+		HELIUM_ASSERT( m_pGlfwWindow != NULL );
+		int width, height;
+		glfwGetWindowSize(m_pGlfwWindow, &width, &height);
+		glBindRenderbuffer( GL_RENDERBUFFER, newRenderbuffer );
+		glRenderbufferStorage( GL_RENDERBUFFER, GL_RGBA8, width, height );
+
+		// Restore previous renderbuffer.
+		glBindRenderbuffer( GL_RENDERBUFFER, curRenderbuffer );
+
+		// Construct the GLSurface object.
+		m_spBackBufferSurface = new GLSurface( newRenderbuffer );
+		HELIUM_ASSERT( m_spBackBufferSurface != NULL );
 	}
 
 	return m_spBackBufferSurface;

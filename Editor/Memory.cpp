f13ad641f51991835f6c51147bfd94eadc05c059
Geoff Evans <geoff@flummoxed.org>
2014-02-16 20:26:53 -0800

    fix build issue with heap flags in Editor

--- Editor/Memory.cpp
+++ Editor/Memory.cpp
@@ -2,10 +2,9 @@
 
 #include "Platform/MemoryHeap.h"
 
-// Define the memory heap for the current module and include the "new"/"delete" operator implementations.
-HELIUM_DEFINE_DEFAULT_MODULE_HEAP( Editor );
-
 #if HELIUM_HEAP
+
+HELIUM_DEFINE_DEFAULT_MODULE_HEAP( Editor );
 
 #if HELIUM_SHARED
 #include "Platform/NewDelete.h"

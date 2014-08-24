11fb82f67dc6d372d6b821c25a6ca1e8c0fd9542
Geoff Evans <geoff@flummoxed.org>
2014-08-23 18:11:11 -0700

    delete ExamplePlugin, tidy up SideScroller

--- Plugins/ExamplePlugin/ExamplePluginPch.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ExamplePlugin/ExamplePlugin.h"
-
-#include "Platform/Assert.h"
-#include "Platform/Trace.h"
-#include "Platform/Memory.h"
-#include "Framework/Components.h"
-#include "Engine/Asset.h"

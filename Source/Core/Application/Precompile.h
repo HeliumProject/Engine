eac6dbad72ca54ca274c446ef70c9257bda9c0ae
Geoff Evans <geoff@flummoxed.org>
2018-04-28 16:02:56 -0700

    Remove old Core modules and code

--- Source/Core/Application/Precompile.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Platform/System.h"
-#include "Platform/Assert.h"
-#include "Platform/MemoryHeap.h"
-#include "Platform/Trace.h"
--- Source/Core/Application/Preferences.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Application/API.h"
-#include "Foundation/FilePath.h"
-
-namespace Helium
-{
-    HELIUM_APPLICATION_API bool GetPreferencesDirectory( Helium::FilePath& preferencesDirectory );
-}

6bf24a46e4790bc5f919b9a58e0f0d8210141828
Geoff Evans <geoff@flummoxed.org>
2018-05-27 20:46:36 -0700

    Finish cleaning up test data, move bull into spearate test project (can't make it work, not sure why, on the list

---
+++ Projects/TestBull/Source/Main/Main.cpp
@@ -0,0 +1,132 @@
+#include "Precompile.h"
+
+#include "Components/Precompile.h"
+#include "EditorSupport/Precompile.h"
+#include "Bullet/Precompile.h"
+
+#include "Ois/OisSystem.h"
+
+#include "Engine/FileLocations.h"
+#include "Framework/SceneDefinition.h"
+#include "Framework/WorldManager.h"
+
+#include "Rendering/Renderer.h"
+#include "Windowing/Window.h"
+
+#include "GameLibrary/Graphics/Sprite.h"
+
+#include "Bullet/BulletEngine.h"
+#include "Bullet/BulletWorld.h"
+#include "Bullet/BulletWorldDefinition.h"
+#include "Bullet/BulletBodyDefinition.h"
+#include "Bullet/BulletShapes.h"
+#include "Bullet/BulletBody.h"
+#include "Bullet/BulletWorldComponent.h"
+
+#include "Persist/ArchiveJson.h"
+#include "Foundation/Log.h"
+
+#include "Framework/ParameterSet.h"
+
+#include "GameLibrary/Graphics/ScreenSpaceText.h"
+
+using namespace Helium;
+
+/// Windows application entry point.
+///
+/// @param[in] hInstance      Handle to the current instance of the application.
+/// @param[in] hPrevInstance  Handle to the previous instance of the application (always null; ignored).
+/// @param[in] lpCmdLine      Command line for the application, excluding the program name.
+/// @param[in] nCmdShow       Flags specifying how the application window should be shown.
+///
+/// @return  Result code of the application.
+#if HELIUM_OS_WIN
+int APIENTRY WinMain( HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPSTR /*lpCmdLine*/, int nCmdShow )
+#else
+int main( int argc, const char* argv[] )
+#endif
+{
+#ifdef HELIUM_DEBUG
+	HELIUM_TRACE_SET_LEVEL( TraceLevels::Debug );
+	Log::EnableStream( Log::Streams::Debug, true );
+#endif
+
+	int32_t result = 0;
+
+	{
+		// Initialize a GameSystem instance.
+		MemoryHeapPreInitializationImpl memoryHeapPreInitialization;
+		AssetLoaderInitializationImpl assetLoaderInitialization;
+		ConfigInitializationImpl configInitialization;
+#if HELIUM_DIRECT3D
+		WindowManagerInitializationImpl windowManagerInitialization( hInstance, nCmdShow );
+#else
+		WindowManagerInitializationImpl windowManagerInitialization;
+#endif
+		RendererInitializationImpl rendererInitialization;
+		AssetPath systemDefinitionPath( "/System:System" );
+
+		FilePath base ( __FILE__ );
+		base = base.Directory().Parent().Parent();
+		std::string fullPath;
+		Helium::GetFullPath( base.Data(), fullPath );
+		base.Set( fullPath );
+		FileLocations::SetBaseDirectory( base );
+
+		GameSystem::Startup();
+		GameSystem* pGameSystem = GameSystem::GetInstance();
+		HELIUM_ASSERT( pGameSystem );
+		bool bSystemInitSuccess = pGameSystem->Initialize(
+			memoryHeapPreInitialization,
+			assetLoaderInitialization,
+			configInitialization,
+			windowManagerInitialization,
+			rendererInitialization,
+			systemDefinitionPath);
+		
+		if( bSystemInitSuccess )
+		{
+			World *pWorld = NULL; 
+
+			{
+				AssetLoader *pAssetLoader = AssetLoader::GetInstance();
+				SceneDefinitionPtr spSceneDefinition;
+
+				AssetPath scenePath( "/Scenes/TestScene:SceneDefinition" );
+				pAssetLoader->LoadObject(scenePath, spSceneDefinition );
+
+				HELIUM_ASSERT( !spSceneDefinition->GetAllFlagsSet( Asset::FLAG_BROKEN ) );
+
+				pWorld = pGameSystem->LoadScene(spSceneDefinition.Get());
+			}
+
+			HELIUM_ASSERT( pWorld );
+
+			if ( pWorld )
+			{
+				Window::NativeHandle windowHandle = rendererInitialization.GetMainWindow()->GetNativeHandle();
+				Input::Initialize(windowHandle, false);
+				Input::SetWindowSize( 
+					rendererInitialization.GetMainWindow()->GetWidth(),
+					rendererInitialization.GetMainWindow()->GetHeight());
+
+				// Run the application.
+				result = pGameSystem->Run();
+			}
+		}
+
+		// Shut down and destroy the system.
+		pGameSystem->Shutdown();
+		GameSystem::Shutdown();
+	}
+
+	// Perform final cleanup.
+	ThreadLocalStackAllocator::ReleaseMemoryHeap();
+
+#if HELIUM_ENABLE_MEMORY_TRACKING
+	DynamicMemoryHeap::LogMemoryStats();
+	ThreadLocalStackAllocator::ReleaseMemoryHeap();
+#endif
+
+	return result;
+}
---
+++ Projects/TestBull/Source/Module/Precompile.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Platform/Trace.h"
+#include "Framework/GameSystem.h"
+#include "FrameworkImpl/MemoryHeapPreInitializationImpl.h"
+#include "FrameworkImpl/AssetLoaderInitializationImpl.h"
+#include "FrameworkImpl/ConfigInitializationImpl.h"
+#include "FrameworkImpl/WindowManagerInitializationImpl.h"
+#include "FrameworkImpl/RendererInitializationImpl.h"
+#include "Foundation/FilePath.h"
+#include "Engine/FileLocations.h"
+#include "Engine/CacheManager.h"
+#include "GameLibrary/Precompile.h"
